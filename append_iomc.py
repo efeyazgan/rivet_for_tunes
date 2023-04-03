aa7587b721b5950c06d27ed3298dec32d32c6c84
Efe Yazgan <efe.yazgan@cern.ch>
2023-04-03 13:09:46 +0200

    Create append_iomc.py

---
+++ append_iomc.py
@@ -0,0 +1,15 @@
+import sys
+inFile = sys.argv[1]
+with open(inFile, 'r+') as f:
+    lines = f.readlines()
+    for i, line in enumerate(lines):
+        if "comEnergy" in line:
+            line = line.replace(7000,13000)
+        if line.startswith('process.source'):
+            lines[i] = lines[i].strip() + '\n\n' + \
+                  "from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper" + '\n' + \
+                  "randSvc = RandomNumberServiceHelper(process.RandomNumberGeneratorService)" + '\n' + \
+                  "randSvc.populate()" + '\n'
+    f.seek(0)
+    for line in lines:
+        f.write(line)
