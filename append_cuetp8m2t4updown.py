c1f03649923620ccc0cde9c9e7372bbdfd648241
Efe Yazgan <efe.yazgan@cern.ch>
2023-04-03 13:30:46 +0200

    Update append_cuetp8m2t4updown.py

--- append_cuetp8m2t4updown.py
+++ append_cuetp8m2t4updown.py
@@ -7,24 +7,24 @@ with open(inFile, 'r+') as f:
             lines[i] = lines[i].replace("),",",")
             if "down" in inFile.lower():
                 lines[i] = lines[i].strip() + '\n' + \
-                "            'Tune:pp 14',"+"\n" + \
-                "            'Tune:ee 7',"+"\n" + \
-                "            'MultipartonInteractions:ecmPow=0.25208',"+"\n" + \
-                "            'SpaceShower:alphaSvalue=0.1108',"+"\n" + \
-                "            'PDF:pSet=LHAPDF6:NNPDF30_lo_as_0130',"+"\n" + \
-                "            'MultipartonInteractions:pT0Ref=2.268694e+00',"+"\n" + \
-                "            'MultipartonInteractions:expPow=1.561995e+00',"+"\n" + \
-                "            'ColourReconnection:range=8.714042e+00'),"
+                "            'Tune:pp 14',\n" + \
+                "            'Tune:ee 7',\n" + \
+                "            'MultipartonInteractions:ecmPow=0.25208',\n" + \
+                "            'SpaceShower:alphaSvalue=0.1108',\n" + \
+                "            'PDF:pSet=LHAPDF6:NNPDF30_lo_as_0130',\n" + \
+                "            'MultipartonInteractions:pT0Ref=2.268694e+00',\n" + \
+                "            'MultipartonInteractions:expPow=1.561995e+00',\n" + \
+                "            'ColourReconnection:range=8.714042e+00'),\n"
             if "up" in inFile.lower():
                 lines[i] = lines[i].strip() + '\n' + \
-                "            'Tune:pp 14',"+"\n" + \
-                "            'Tune:ee 7',"+"\n" + \
-                "            'MultipartonInteractions:ecmPow=0.25208',"+"\n" + \
-                "            'SpaceShower:alphaSvalue=0.1108',"+"\n" + \
-                "            'PDF:pSet=LHAPDF6:NNPDF30_lo_as_0130',"+"\n" + \
-                "            'MultipartonInteractions:pT0Ref=2.127913e+00',"+"\n" + \
-                "            'MultipartonInteractions:expPow=1.710694e+00',"+"\n" + \
-                "            'ColourReconnection:range=6.500048e+00'),"
+                "            'Tune:pp 14',\n" + \
+                "            'Tune:ee 7',\n" + \
+                "            'MultipartonInteractions:ecmPow=0.25208',\n" + \
+                "            'SpaceShower:alphaSvalue=0.1108',\n" + \
+                "            'PDF:pSet=LHAPDF6:NNPDF30_lo_as_0130',\n" + \
+                "            'MultipartonInteractions:pT0Ref=2.127913e+00',\n" + \
+                "            'MultipartonInteractions:expPow=1.710694e+00',\n" + \
+                "            'ColourReconnection:range=6.500048e+00'),\n"
     f.seek(0)
     for line in lines:
         f.write(line)
