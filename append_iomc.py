56ebb2b23caf0bcb4cde3915e5e9a25b4f95d288
Efe Yazgan <efe.yazgan@cern.ch>
2023-04-03 14:10:03 +0200

    Update append_iomc.py

--- append_iomc.py
+++ append_iomc.py
@@ -4,7 +4,7 @@ with open(inFile, 'r+') as f:
     lines = f.readlines()
     for i, line in enumerate(lines):
         if "comEnergy" in line:
-            line = line.replace(7000,13000)
+            line = line.replace("7000","13000")
         if line.startswith('process.source'):
             lines[i] = lines[i].strip() + '\n\n' + \
                   "from IOMC.RandomEngine.RandomServiceHelper import RandomNumberServiceHelper" + '\n' + \
